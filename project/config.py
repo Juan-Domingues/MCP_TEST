eaaee14d06ea57c17602e8c431bcf1b5d4712ad2
Juan Domingues <jp291103@gmail.com>
2026-04-21 10:16:56 -0300

    Replace sensitive ClickUp credentials with placeholders

--- project/config.py
+++ project/config.py
@@ -2,7 +2,7 @@ import os
 
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-CLICKUP_TOKEN = "pk_296467621_M4SC8JWUA6VY2L6HATR38129VMY4Y7TD"
-LIST_ID = "901713120264"
+CLICKUP_TOKEN = "API_KEY"
+LIST_ID = "ID_LIST"
 
 EXCEL_FILE = os.path.join(BASE_DIR, "projetos.xlsx")
